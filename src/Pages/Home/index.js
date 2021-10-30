01265a903bb3136f6ee47bf3611e25d2efd9203c
Matheus de Carvalho Jesus <euescolhoesse@gmail.com>
2021-10-30 02:14:26 -0300

    Criado a tela principal de exibição dos repositorios

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,34 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
+import Menubar from '../../components/MenuBar'
+import api from '../../service/api'
+import { CardRepositorios } from './Components/CardRepositorios'
 
 export function Home () {
+  const [repositorios, setRepositorios] = React.useState([])
+  function handleClick () {
+    console.log('clicked')
+    api.get('repositories?q=facebook&sort=stars&order=desc&per_page=50&page=2').then(res => {
+      console.log(res.data)
+      setRepositorios(res.data.items)
+    })
+  }
   return (
     <div>
-      <h1>Home</h1>
+      <Menubar />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={3}>
+          <h1>Home</h1>
+          <button onClick={handleClick}>Click</button>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <Grid container direction='column' spacing={3}>
+            <Grid item xs={12}>
+              <CardRepositorios repositorios={repositorios} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   )
 }

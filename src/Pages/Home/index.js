112c5da9e98b210ceee07942c950d2257c82eca5
Matheus de Carvalho Jesus <euescolhoesse@gmail.com>
2021-10-31 00:27:56 -0300

    Ajustado a tela inicial para a paginação

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,29 +1,35 @@
 import { Grid } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Menubar from '../../components/MenuBar'
 import api from '../../service/api'
 import { CardRepositorios } from './Components/CardRepositorios'
+import Pagination from '../../components/Pagination'
 
 export function Home () {
   const [repositorios, setRepositorios] = useState([])
   const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+  const [total, setTotal] = useState(0)
 
-  function handleClick (string, language) {
+  function handleSearch () {
     const searchString = search.replace(/\s/g, '+')
-    api.get(`repositories?q=${searchString}&order=desc&per_page=50&page=1`).then(res => {
+    api.get(`repositories?q=${searchString}&order=desc&per_page=50&page=${page}`).then(res => {
+      console.log(res.data)
       setRepositorios(res.data.items)
+      setTotal(Math.ceil(res.data.total_count / res.data.items.length))
     })
   }
+
+  useEffect(() => {
+    handleSearch()
+  }, [page])
   return (
     <div>
-      <Menubar search={search} setSearch={setSearch} />
+      <Menubar setPage={setPage} setSearch={setSearch} handleSearch={handleSearch} />
       <Grid container spacing={3}>
-        <Grid item xs={12} md={3}>
-          <h1>Home</h1>
-          <button onClick={() => handleClick('facebook', 'Python')}>Click</button>
-        </Grid>
-        <Grid item xs={12} md={9}>
+        <Grid item xs={12} md={12}>
           <Grid container direction='column' spacing={2}>
+            <Pagination maxPages={total} page={page} setPage={setPage} />
             {repositorios.map(repo => (
               <Grid item xs={12} key={repo.id}>
                 <CardRepositorios repo={repo} />
